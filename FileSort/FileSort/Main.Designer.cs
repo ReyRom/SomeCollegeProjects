13ff9883883fe3b7c8a1452010bedd91f101da28
ReyRom <41283862+ReyRom@users.noreply.github.com>
2022-11-08 17:49:08 +0300

    sort

---
+++ FileSort/FileSort/Main.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace FileSort
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.sortButton = new System.Windows.Forms.Button();
+            this.modeComboBox = new System.Windows.Forms.ComboBox();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.label1 = new System.Windows.Forms.Label();
+            this.includeCheckBox = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // folderTextBox
+            // 
+            this.folderTextBox.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.folderTextBox.Location = new System.Drawing.Point(28, 28);
+            this.folderTextBox.Name = "folderTextBox";
+            this.folderTextBox.Size = new System.Drawing.Size(628, 33);
+            this.folderTextBox.TabIndex = 0;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.searchButton.Location = new System.Drawing.Point(672, 28);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(106, 33);
+            this.searchButton.TabIndex = 1;
+            this.searchButton.Text = "Обзор...";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // sortButton
+            // 
+            this.sortButton.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.sortButton.Location = new System.Drawing.Point(613, 83);
+            this.sortButton.Name = "sortButton";
+            this.sortButton.Size = new System.Drawing.Size(165, 49);
+            this.sortButton.TabIndex = 2;
+            this.sortButton.Text = "Сортировать";
+            this.sortButton.UseVisualStyleBackColor = true;
+            this.sortButton.Click += new System.EventHandler(this.SortButton_Click);
+            // 
+            // modeComboBox
+            // 
+            this.modeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.modeComboBox.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.modeComboBox.FormattingEnabled = true;
+            this.modeComboBox.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.modeComboBox.Items.AddRange(new object[] {
+            "по дате",
+            "по учебному году",
+            "по расширению",
+            "по типу файла",
+            "по размеру"});
+            this.modeComboBox.Location = new System.Drawing.Point(245, 71);
+            this.modeComboBox.Name = "modeComboBox";
+            this.modeComboBox.Size = new System.Drawing.Size(320, 33);
+            this.modeComboBox.TabIndex = 3;
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Location = new System.Drawing.Point(0, 157);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(807, 22);
+            this.statusStrip.TabIndex = 4;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(79, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Тип сортировки";
+            // 
+            // includeCheckBox
+            // 
+            this.includeCheckBox.AutoSize = true;
+            this.includeCheckBox.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.includeCheckBox.Location = new System.Drawing.Point(95, 114);
+            this.includeCheckBox.Name = "includeCheckBox";
+            this.includeCheckBox.Size = new System.Drawing.Size(422, 29);
+            this.includeCheckBox.TabIndex = 6;
+            this.includeCheckBox.Text = "сортировать файлы во вложенных каталогах";
+            this.includeCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(28, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Каталог для сортировки";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 179);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.includeCheckBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.modeComboBox);
+            this.Controls.Add(this.sortButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.folderTextBox);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "Sorting";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox folderTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button sortButton;
+        private System.Windows.Forms.ComboBox modeComboBox;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox includeCheckBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FileSort/FileSort/Main.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileSort
+{
+    public partial class Main : Form
+    {
+        delegate void Sorting(FileInfo file);       //объявление делегата, используемого для хранения ссылок на методы сортировки
+        
+        int errorsCounter = 0;
+        int fileCounter = 0;
+
+        [DllImport("DLLSORT.dll", CallingConvention = CallingConvention.StdCall)]       //объявление функции из библиотеки DLLSORT.dll, написанной на C++
+        static extern IntPtr size_sort(long fileSize);
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();  // создание объекта диалогового окна выбора каталога
+            if (folderBrowser.ShowDialog() == DialogResult.OK)              
+            {
+                folderTextBox.Text = folderBrowser.SelectedPath;            // запись пути к каталогу в текстовое поле
+            }
+        }
+
+        private void SortButton_Click(object sender, EventArgs e)
+        {
+            string path = folderTextBox.Text;
+            if (Directory.Exists(path))
+            {
+                DirectoryInfo directory = new DirectoryInfo(path);
+                Sorting sorting;
+                switch (modeComboBox.SelectedIndex)
+                {
+                    case 0:
+                        {
+                            sorting = (file) => DateSorting(file);          // присвоение делегату ссылки на метод
+                            break;
+                        }
+                    case 1:
+                        {
+                            sorting = (file) => StudYearSorting(file);
+                            break;
+                        }
+                    case 2:
+                        {
+                            sorting = (file) => ExtensionSorting(file);
+                            break;
+                        }
+                    case 3:
+                        {
+                            sorting = (file) => TypeSorting(file);
+                            break;
+                        }
+                    case 4:
+                        {
+                            sorting = (file) => SizeSorting(file);
+                            break;
+                        }
+                    default:
+                        {
+                            MessageBox.Show("Необходимо выбрать тип сортировки");
+                            return;
+                        }
+                }
+
+                SearchOption option;
+                if (includeCheckBox.Checked)
+                {
+                    option = SearchOption.AllDirectories;
+                }
+                else
+                {
+                    option = SearchOption.TopDirectoryOnly;
+                }
+
+                FileInfo[] files = directory.GetFiles("*.*", option);
+
+                Task[] tasks = new Task[files.Length];          // объявление массива задач
+                
+                foreach (var file in files)
+                {
+                    tasks[fileCounter] = Task.Factory.StartNew(() => sorting(file));   // запуск задач в массиве и вызов делегата содержащего ссылку на один из методов сортировки
+                    fileCounter++;
+                }                                                                       
+                Task.WaitAll(tasks);
+                statusStrip.Items.Clear();
+                statusStrip.Items.Add($"Отсортировано {files.Length-errorsCounter} из {files.Length} файлов");
+                errorsCounter = 0;
+                fileCounter = 0;
+            }
+            else
+            { 
+                MessageBox.Show("Данный каталог не существует"); 
+            }
+        }
+
+        /// <summary>
+        /// Сортровка файла в каталог, исходя из его размера
+        /// </summary>
+        /// <param name="file">объект файла</param>
+        void SizeSorting(FileInfo file)
+        {
+            string targetPath = $@"{file.Directory}\{Marshal.PtrToStringAnsi(size_sort(file.Length))}";
+            MoveFile(file, targetPath);
+        }
+
+        /// <summary>
+        /// Сортровка файла в каталог, исходя из даты его создания с учетом учебного года
+        /// </summary>
+        /// <param name="file">объект файла</param>
+        void StudYearSorting(FileInfo file)
+        {
+            string targetPath;
+            DateTime writingData = file.CreationTime;
+            DateTime studYearStart = new DateTime(writingData.Year, 9, 1);
+            if (writingData >= studYearStart)
+            {
+                targetPath = $@"{file.Directory}\{writingData.Year}-{writingData.Year+1} уч. год";
+            }
+            else
+            {
+                targetPath = $@"{file.Directory}\{writingData.Year - 1}-{writingData.Year} уч. год";
+            }
+            MoveFile(file, targetPath);
+        }
+
+        /// <summary>
+        /// Сортровка файла в каталог, исходя из даты его создания
+        /// </summary>
+        /// <param name="file">объект файла</param>
+        void DateSorting(FileInfo file)
+        {
+            DateTime writingData = file.CreationTime;
+            string targetPath = $@"{file.Directory}\{writingData.Year}\{writingData.ToString("MMMM", CultureInfo.CreateSpecificCulture("ru-RU"))}\{writingData.Day}";
+            MoveFile(file, targetPath);
+        }
+
+        /// <summary>
+        /// Сортровка файла в каталог, исходя из его расширения
+        /// </summary>
+        /// <param name="file">объект файла</param>
+        void ExtensionSorting(FileInfo file)
+        {
+            string targetPath = $@"{file.Directory}\{file.Extension}";
+            MoveFile(file, targetPath);
+        }
+
+        /// <summary>
+        /// Сортровка файла в каталог, исходя из его типа
+        /// </summary>
+        /// <param name="file">объект файла</param>
+        void TypeSorting(FileInfo file)
+        {
+            string folder;
+            switch (file.Extension)
+            {
+                case ".jpeg": case ".jpg": case ".bmp": case ".gif": case ".tiff": case ".png":
+                    {
+                        folder = "Изображения";
+                        break; 
+                    }
+                case ".txt": case ".rtf": case ".doc": case ".docx":
+                    {
+                        folder = "Текстовые файлы";
+                        break;
+                    }
+                case ".xls": case ".xlsx": case ".xlsm": case ".ods":
+                    {
+                        folder = "Таблицы";
+                        break;
+                    }
+                case ".avi": case ".wmf": case ".3gp": case ".mp4": case ".mpg2": case ".mkv":
+                    {
+                        folder = "Видео";
+                        break;
+                    }
+                case ".mp3": case ".wma":
+                    {
+                        folder = "Аудио";
+                        break;
+                    }
+                case ".zip": case ".rar": case ".7z": case ".tg":
+                    {
+                        folder = "Архивы";
+                        break;
+                    } 
+                case ".exe": case ".cmd": case ".bat": case ".msi":
+                    {
+                        folder = "Исполняемые файлы";
+                        break;
+                    } 
+                default:
+                    {
+                        folder = "Другое";
+                        break;
+                    }
+            }
+            string targetPath = $@"{file.Directory}\{folder}";
+            MoveFile(file, targetPath);
+        }
+
+        /// <summary>
+        /// Перемещение файлов в целевые каталоги
+        /// </summary>
+        /// <param name="file">объект файла</param>
+        /// <param name="targetPath">имя целевого каталога</param>
+        void MoveFile(FileInfo file, string targetPath)
+        {
+            if (!Directory.Exists(targetPath))
+            {
+                Directory.CreateDirectory(targetPath);          //создание каталога, если он не существует
+            }
+            try
+            {
+                file.MoveTo($@"{targetPath}\{file.Name}");      //перемещение файла
+            }
+            catch (Exception)
+            {
+                errorsCounter++;                                //увеличение счетчика ошибок, при их возникновении
+            }
+        }
+    }
+}
